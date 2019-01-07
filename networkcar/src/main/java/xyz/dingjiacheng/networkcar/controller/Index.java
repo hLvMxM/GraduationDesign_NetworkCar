335a017090aa1af75cfae3de359875b686ef0760
Unknown <308370346@qq.com>
2019-01-07 21:40:51 +0800

    update project
    成功显示页面

--- networkcar/src/main/java/xyz/dingjiacheng/networkcar/controller/Index.java
+++ networkcar/src/main/java/xyz/dingjiacheng/networkcar/controller/Index.java
@@ -1,13 +1,15 @@
 package xyz.dingjiacheng.networkcar.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Index {
 	
-	@RequestMapping("/index")
+	@GetMapping("/index")
 	public String index() {
-		return "hello";
+		return "index";
 	}
 }
