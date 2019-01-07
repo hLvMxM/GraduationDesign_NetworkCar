aef4d9b9192876d5edf1dda63ee2926f9b64f8d0
Unknown <308370346@qq.com>
2019-01-07 20:53:01 +0800

    use spring
    use spring

---
+++ networkcar/src/main/java/xyz/dingjiacheng/networkcar/NetworkcarApplication.java
@@ -0,0 +1,14 @@
+package xyz.dingjiacheng.networkcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkcarApplication.class, args);
+	}
+
+}
+
