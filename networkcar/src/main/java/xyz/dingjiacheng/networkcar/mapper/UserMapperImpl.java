a85d21da91269db816d9624ef1f81faaf0f8a265
Unknown <308370346@qq.com>
2019-01-12 21:13:08 +0800

    update security
    实现用户登陆，密码确认

--- networkcar/src/main/java/xyz/dingjiacheng/networkcar/mapper/UserMapperImpl.java
+++ networkcar/src/main/java/xyz/dingjiacheng/networkcar/mapper/UserMapperImpl.java
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 import xyz.dingjiacheng.networkcar.model.User;
 import xyz.dingjiacheng.networkcar.util.DBUtil;
 
 public class UserMapperImpl implements UserMapper{
 
+	
+	
 	@Override
 	public User selectById(String id) {
 		User user = null;
@@ -39,24 +40,29 @@ public class UserMapperImpl implements UserMapper{
 	@Override
 	public User selectByUsername(String username) {
 		User user = null;
+		String password = "";
 		try {
-		Connection conn = DBUtil.getConnection();
-		String sql = "select * from user where username = ?";
-		PreparedStatement pst = conn.prepareStatement(sql);
-		pst.setString(1, username);
-		ResultSet rs;
-		rs = pst.executeQuery();
-		if(rs.next()) {
-			user = new User(rs.getString(2),
-							rs.getString(3),
-							rs.getString(4));
-		}
+			Connection conn = DBUtil.getConnection();
+			String sql = "select * from user where username = ?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, username);
+			ResultSet rs;
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				password = (new BCryptPasswordEncoder().encode(rs.getString(3).trim()));
+				user = new User(rs.getString(2),
+								password,
+								rs.getString(4));
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String password = (new BCryptPasswordEncoder().encode("123456".trim()));
-		return new User("123123",password,"admin");
+//		if (user==null) {
+//			logger.info("null");
+//		}else {
+//		    logger.info("password: {}", password);
+//		}
+		return user;
 	}
 
 }
--- networkcar/src/main/java/xyz/dingjiacheng/networkcar/service/UserService.java
+++ networkcar/src/main/java/xyz/dingjiacheng/networkcar/service/UserService.java
@@ -1,5 +1,7 @@
 package xyz.dingjiacheng.networkcar.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,10 +14,14 @@ import xyz.dingjiacheng.networkcar.model.User;
 @Service
 public class UserService implements UserDetailsService {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	private UserMapper usermapper = new UserMapperImpl();
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		logger.info("用户的用户名: {}", username);
 		User user = usermapper.selectByUsername(username);
 		if(user==null) {
 			throw new UsernameNotFoundException(String.format("User with username=%s was not found", username));
